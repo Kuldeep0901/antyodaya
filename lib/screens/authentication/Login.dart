b57d18889ccf946d09c2b1e0ae008e071a21b73c
srijan-bytes <59538422+srijan-bytes@users.noreply.github.com>
2021-04-09 18:22:20 +0530

    Revert "Made Profile and modified pubspec.yaml and other files"

--- lib/screens/authentication/Login.dart
+++ lib/screens/authentication/Login.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'SignUp.dart';
-import 'dart:ui';
 
 class Login extends StatefulWidget {
   @override
